fd567d59e31258d9e73f2a7f3d5284860895f783
shenjunjian <40288193@qq.com>
2026-07-23 20:12:11 +0800

    fix: 增加ts 声明文件

---
+++ packages/pure-vapor/backup-ts/vue.d.ts
@@ -0,0 +1,27 @@
+import type { CompilerOptions } from '@vue/compiler-dom'
+import {
+  type RenderFunction,
+  defineAsyncComponent as defineVaporAsyncComponent$1,
+} from '@vue/runtime-dom'
+import {
+  type VaporSlot,
+  defineVaporAsyncComponent,
+  withAsyncContext,
+} from '@vue/runtime-vapor'
+export * from '@vue/runtime-dom'
+export * from '@vue/runtime-vapor'
+
+//#region \0rolldown/runtime.js
+//#endregion
+//#region temp/packages/vue/src/index.d.ts
+export declare function compileToFunction(
+  template: string | HTMLElement,
+  options?: CompilerOptions,
+): RenderFunction
+//#endregion
+export {
+  type VaporSlot,
+  compileToFunction as compile,
+  defineVaporAsyncComponent,
+  withAsyncContext,
+}
